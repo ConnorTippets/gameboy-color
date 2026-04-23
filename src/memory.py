2d4852f308299e576b32658e2056cb10647fee33
ConnorTippets <meiscool466@gmail.com>
2026-04-23 10:19:56 -0500

    feat: memory mapped read

--- src/memory.py
+++ src/memory.py
@@ -1,4 +1,12 @@
-WRAM_SIZE = 32 * 1024
+# i'll handle bank switching later
+WRAM_SIZE = 8 * 1024
+DEFAULT_WRAM_START = 0xC000
+
+ROM_SIZE = 32 * 1024
+DEFAULT_ROM_START = 0x0000
+
+ECHO_START = 0xE000
+ECHO_END = 0xFDFF
 
 
 class Readable:
@@ -15,19 +23,52 @@ class Writeable:
         self.buf[addr] = val
 
 
-class WRAM(Readable, Writeable):
+class MemoryMapped:
+    start: int
+    size: int
+
+    @property
+    def end(self):
+        return self.start + self.size
+
+
+class WRAM(Readable, Writeable, MemoryMapped):
     def __init__(self):
         self.buf = bytearray(WRAM_SIZE)
+        self.start = DEFAULT_WRAM_START
+        self.size = WRAM_SIZE
 
 
-class ROM(Readable):
+class ROM(Readable, MemoryMapped):
     def __init__(self):
-        self.buf = bytearray()
+        self.buf = bytearray(ROM_SIZE)
+        self.start = DEFAULT_ROM_START
+        self.size = ROM_SIZE
 
     @classmethod
     def load(cls, path: str):
-        rom = cls()
+        rom_buf = bytearray()
+
         with open(path, "rb") as handle:
-            rom.buf = bytearray(handle.read())
+            rom_buf = bytearray(handle.read())
+
+        rom = cls()
+        rom.buf[: len(rom_buf)] = rom_buf
 
         return rom
+
+
+class Memory:
+    def __init__(self):
+        self.rom = ROM()
+        self.wram = WRAM()
+
+    def read_byte(self, addr: int) -> int:
+        if self.rom.start <= addr and addr < self.rom.end:
+            return self.rom.read_byte(addr - self.rom.start)
+        elif self.wram.start <= addr and addr < self.wram.end:
+            return self.wram.read_byte(addr - self.wram.start)
+        elif ECHO_START <= addr and addr < ECHO_END:
+            return self.wram.read_byte(addr - ECHO_START)
+        else:
+            raise Exception(f"Invalid mem read: {hex(addr).upper()[2:]}")
